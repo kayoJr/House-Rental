fd3c41aca40c0c010cfddcf378590f748b964b30
kayoJr <kalebmelaku97@gmail.com>
2022-05-04 11:01:24 +0300

    responsive for home

--- JS/app.js
+++ JS/app.js
@@ -9,3 +9,25 @@ function scrollFunction() {
     navbar.classList.remove("sticky");
     }
 }
+
+var ham = document.getElementById('ham');
+var menu = document.getElementById('menu');
+var avatar = document.getElementById('avatar');
+var close = document.querySelectorAll('.close');
+var second_navbar = document.getElementById('second-nav');
+
+ham.addEventListener("click", ()=>{
+        menu.style.display = "block"
+     //   body.style.display = "none"
+})
+
+close.forEach(function (exit) {
+        exit.addEventListener("click", () => {
+            menu.style.display = "none";
+            second_navbar.style.display = "none";
+        });
+    })
+
+avatar.addEventListener("click", ()=>{
+    second_navbar.style.display = 'block';
+})
